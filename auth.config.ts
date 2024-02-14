34837718a53b0c909834bbd76925148286137fb5
PakawatJaroenying <lookjunhomerun@gmail.com>
2024-02-14 15:38:13 +0700

    added auth

--- auth.config.ts
+++ auth.config.ts
@@ -10,13 +10,12 @@ export const authConfig : NextAuthConfig = {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if(isOnDashboard){
-                console.log("🚀 ~ authorized ~ isOnDashboard:", isOnDashboard)
                 if(isLoggedIn) return true;
                 return false;
             }else if(isLoggedIn){
-                console.log("🚀 ~ authorized ~ isLoggedIn:", isLoggedIn)
-                console.log("🚀 ~ authorized ~ nextUrl:", nextUrl)
-                return Response.redirect(new URL('/dashboard' , nextUrl));
+               
+                // return Response.redirect(new URL('/dashboard' , nextUrl));
+                return true;
             }
             return true;
         }
